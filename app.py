1caf7bdf90857bcac96662c0766aa7645b728bfb
istrebitel-1 <istrebitel.3.12@gmail.com>
2020-12-03 23:05:10 +0300

    add route

--- app.py
+++ app.py
@@ -10,18 +10,18 @@ def index():
     return render_template("index.html")
 
 #национальные проекты
-@app.route('/db_natdata',methods=['GET'])
-def database_R():
-	cursor.execute("select np.project_name||'$'||round(cast(np.project_budget/10000000000 as numeric), 2)||' млрд. руб.$'||count(fp.id_federal_project)||'$'||count(fp.id_federal_project)||'$'||count(fp.id_federal_project)||';'"
-    " from national_projects np"
-    " join federal_projects fp on np.id_national_project = fp.id_national_project "
-    " group by np.project_name, np.project_budget")
+@app.route('/db_natdata', methods=['GET'])
+def database_np():
+    cursor.execute("select np.project_name||'$'||round(cast(np.project_budget/10000000000 as numeric), 2)||' млрд. руб.$'||count(fp.id_federal_project)||'$'||count(fp.id_federal_project)||'$'||count(fp.id_federal_project)||';'"
+        " from national_projects np"
+        " join federal_projects fp on np.id_national_project = fp.id_national_project"
+        " group by np.project_name, np.project_budget")
     results = cursor.fetchall()
     output = ""
     for item in results:
-    for subitem in item:
-        output+=subitem
-	return output
+        for subitem in item:
+            output+=subitem
+    return output
 
 
 if __name__ == "__main__":
