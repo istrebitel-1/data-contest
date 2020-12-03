fd3a20cc22057739325edd5d0b50ab88014dbe86
istrebitel-1 <istrebitel.3.12@gmail.com>
2020-12-03 20:59:57 +0300

    Parser upd

--- parsers/connection.py
+++ parsers/connection.py
@@ -3,6 +3,6 @@ import psycopg2
 conn = psycopg2.connect(database="data_contest", user="postgres", password="4432", host="26.173.145.160", port="5434")
 cursor = conn.cursor()
 
-# cursor.execute("select * from national_projects")
-# results = cursor.fetchall()
-# print(results)
+cursor.execute("select * from national_projects")
+results = cursor.fetchall()
+print(results)
--- parsers/parser.py
+++ parsers/parser.py
@@ -1,56 +1,61 @@
 import requests
 from bs4 import BeautifulSoup
-
+import time
+import json
 #custom
 from connection import conn, cursor
 
 
 #functions
-def get_national_projects_info():
-  url = 'https://spending.gov.ru/np/' # url страницы
-  r = requests.get(url)
-  with open('parse.html', 'w', encoding="utf-8") as output_file:
-    output_file.write(r.text)
+def get_national_projects_info(url):
+  resp = requests.get(url).text
+  data = json.loads(resp)
 
-  with open("parse.html", "r", encoding="utf-8") as f:
-      contents = f.read()
-      soup = BeautifulSoup(contents, 'lxml')
+  # try:
+  #   cursor.execute("truncate table national_projects")
+  #   conn.commit()
+  # except Exception as e:
+  #   print("Error!", e) 
 
-      national_projects_links = soup.find_all("a", {'class': 'spending-info-card'})
-      national_projects_name = soup.find_all("div", {'class': 'h3 font-weight-bold mb-4'})
-      
+  for index, item in enumerate(data['items']):
+    national_project_name = item['np_short_name']
+
+    try:
+      cursor.execute("insert into national_projects(id_national_project, project_name, project_budjet) values (%i, '%s', '%s')" %(index, national_project_name, 100))
+      conn.commit()
+    except Exception as e:
+      print("Error!", e) 
+
+    for index2, item2 in item['fedprojects']:
+      federal_project_name = item2['fp_fullname']
       try:
-        cursor.execute("truncate table national_projects")
+        cursor.execute("insert into federal_projects(id_federal_project, id_national_project, project_name, project_budget) values (%i, %i, '%s', '%i')" %(index, index2, national_projects_name, 200+index2))
         conn.commit()
       except Exception as e:
-        return print("Error!", e) 
-
-      for i in range(0, len(national_projects_links)):
-        name = national_projects_name[i].text.strip()
-        link_question='https://spending.gov.ru'+national_projects_links[i].get('href').strip()
-        try:
-          cursor.execute("insert into national_projects(id_national_project, project_name, project_link) values (%i, '%s', '%s')" %(int(i+1), name, link_question))
-          conn.commit()
-        except Exception as e:
-          print("Error!", e) 
-          continue
+        print("Error!", e) 
 
 
-#get_national_projects_info()
 
 
-url2 = 'https://futurerussia.gov.ru/' # url страницы
-r = requests.get(url2)
-with open('parse.html', 'w', encoding="utf-8") as output_file:
-  output_file.write(r.text)
+
+url = 'https://api.spending.gov.ru/v1/natprojects/nprojects/'
+get_national_projects_info(url)
+
+# url2 = 'https://futurerussia.gov.ru/' # url страницы
+# r = requests.get(url2)
+# with open('parse.html', 'w', encoding="utf-8") as output_file:
+#   output_file.write(r.text)
 
 
-  with open("parse.html", "r", encoding="utf-8") as f:
-    contents = f.read()
-    soup = BeautifulSoup(contents, 'lxml')
+#   with open("parse.html", "r", encoding="utf-8") as f:
+#     contents = f.read()
+#     soup = BeautifulSoup(contents, 'lxml')
 
-    national_projects_links = soup.find_all("a", {'class': 'ts-slider-projects-item ts-slider-projects__item'})
+#     national_projects_links = soup.find_all("a", {'class': 'ts-slider-projects-item ts-slider-projects__item'})
 
-    for i in range(0, len(national_projects_links)):
-      link_question='https://spending.gov.ru'+national_projects_links[i].get('href').strip()
-      print(link_question)
+#     for i in range(0, len(national_projects_links)):
+#       link_question='https://spending.gov.ru'+national_projects_links[i].get('href').strip()
+#       print(link_question)
+
+# file_path = 'parse.html'
+# os.remove(file_path)
